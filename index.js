bba9fb969e3c9f33f4f097914719c6e98c38c74b
Pedro Augusto <pedrosousaugustooo@gmail.com>
2025-11-28 13:23:59 -0300

    Adiciona interatividade e estilos ao componente de avaliação

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let button_numbers = document.querySelectorAll('.button-number');
+const button_submit = document.getElementById('button-submit');
 
-function active() {
-   
-    button_numbers.classList.add('button-number-active');
+button_submit.addEventListener('click', () => {
+    button_submit.classList.toggle('selecionado');
+    document.querySelector('.container').classList.toggle('primeiro_banner');
+
+})
+
+const button_numbers = [];
+
+for (let i = 1 ; i <= 5 ; i++) {
+    button_numbers.push(document.getElementById(`button-${i}`));
 }
+
+const active = () => {
+    button_numbers.map( button => {
+        button.classList.remove('active');
+    })
+    event.target.classList.add('active');
+}
+
+const selected_rating = document.getElementById('selected-rating');
+
+button_numbers.forEach( (button, index) => {
+    button.addEventListener('click', () => {
+        selected_rating.innerText = `You selected ${index + 1} out of 5`;
+    });
+})
